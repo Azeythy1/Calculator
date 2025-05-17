06ab778a2dc8939f8aa29bc539ce13f65577ea93
Azeythy1 <azeythy1@gmail.com>
2025-05-17 14:58:22 -0300

    ff

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,9 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p className="footer-text">Desenvolvido por [Rodrigo Oliveira],© 2023 Todos os direitos reservados</p>
+        <p className="footer-text">
+            Desenvolvido por [Rodrigo Oliveira],
+            © 2023 Todos os direitos reservados</p>
         <p>Version 17.05.25</p>
       </div>
     </footer>
